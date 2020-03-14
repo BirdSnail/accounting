e86e437e223031c398efbee3805dd5a01b75fba6
yanghuadong <314726028@qq.com>
2020-03-14 14:08:22 +0800

    'initial'

---
+++ src/test/java/com/birdsnail/accouting/AccoutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birdsnail.accouting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccoutingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
